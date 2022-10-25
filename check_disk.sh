6281b6daab5a49fd5017fbe336910c7269eda016
tsd-nghialv <40819125+tsd-nghialv@users.noreply.github.com>
2022-10-25 08:26:55 +0700

    Delete check_disk.sh

--- check_disk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-disk=$(df -h / | egrep -v "Size|Used" | sed -e 's/G//g' | awk '{print $2-$3}')
-#echo $disk
-
-if [ $disk -le 40 ]; then
-	echo -e "Subject: Disk Warning!\nDisk Low is $disk"G"" | sendmail nghialv@thaison.vn
-else
-	exit 0;
-fi

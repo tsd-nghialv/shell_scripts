d5a0ec6bd10b0cf4f4d87e8cd8efe27a6f45f87f
nghialv <luunghianghia@gmail.com>
2022-10-25 11:03:28 +0700

    Edit Script

--- check_ceph.sh
+++ check_ceph.sh
@@ -2,9 +2,9 @@
 status="HEALTH_OK"
 command=$(ceph -s | grep health)
 if [[ -z $status  ]]; then
-token="5520385337:AAGc0Xtsw1xYVYZ0rTzpyrDx-AeVXyf6rh4"
+token="5542561908:AAGGlo2WYnvC_9ONjprPUponitwSEd1pvck"
 chatid="-711836222"
-msg=$(echo "CEPH WARNING!")
+msg=$(echo "CEPH WARNING! $command")
 curl -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id=$chatid -d text="$msg"
 else
 	exit 0;
